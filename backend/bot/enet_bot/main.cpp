1b1ec71ee86a29d79d738cb642d7b97e3137eb67
test <test>
2026-09-23 03:04:53 +0100

    Compile bot with custom Growtopia ENet source

--- backend/bot/enet_bot/main.cpp
+++ backend/bot/enet_bot/main.cpp
@@ -204,6 +204,7 @@ int main() {
     atexit(enet_deinitialize);
 
     ENetHost* client = enet_host_create(NULL, 1, 2, 0, 0);
+    client->usingNewPacket = true;
     client->checksum = enet_crc32;
     enet_host_compress_with_range_coder(client);
 
